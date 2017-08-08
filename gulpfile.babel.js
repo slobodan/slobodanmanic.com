a976ab80c25d936246c3ee18f2d7478ac8a80334
Slobodan Manic <slobodan.manic@gmail.com>
2017-08-08 23:31:15 +0200

    Tests adding download remote file into dist on build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import cp from "child_process";
 import gutil from "gulp-util";
 import postcss from "gulp-postcss";
+import remoteSrc from 'gulp-remote-src';
 import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
@@ -15,8 +16,15 @@ const defaultArgs = ["-d", "../dist", "-s", "site", "-v"];
 gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
 
-gulp.task("build", ["css", "js", "hugo"]);
+gulp.task("build", ["css", "js", "remote", "hugo"]);
 gulp.task("build-preview", ["css", "js", "hugo-preview"]);
+
+gulp.task('remote', function() {
+  remoteSrc(['master.zip'], {
+    base: 'https://github.com/netlify/victor-hugo-styleguide/archive/'
+  })
+  .pipe(gulp.dest('./dist/downloads/'));
+})
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
